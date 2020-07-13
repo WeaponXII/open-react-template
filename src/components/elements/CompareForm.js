352535a390e2e46f397202e7aeaac2623bb1f46a
Jabali Ndungu <jabalin008@gmail.com>
2020-07-13 22:39:05 +0300

    Created Form

---
+++ src/components/elements/CompareForm.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+export default function App() {
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = data => console.log(data);
+  console.log(errors);
+  
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <select name="Compare" ref={register}>
+        <option value="Red">Red</option>
+        <option value="Green">Green</option>
+      </select>
+
+      <input type="submit" />
+    </form>
+  );
+}
